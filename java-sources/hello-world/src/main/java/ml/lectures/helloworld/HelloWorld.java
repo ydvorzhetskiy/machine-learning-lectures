f9030451fc6f259c84792983e9225ff127072a67
o.n.slautin <oslautin@luxoft.com>
2020-02-14 17:45:12 +0600

    initial java source upload

---
+++ java-sources/hello-world/src/main/java/ml/lectures/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package ml.lectures.helloworld;
+
+/**
+ * HelloWord  description
+ *
+ * @author <a href="mailto:oslautin@luxoft.com">Oleg N.Slautin</a>
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+    }
+}
